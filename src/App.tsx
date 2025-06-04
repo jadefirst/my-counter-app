e8acc55eeb9e38b7a6a60f5c9a48c3eb25c62b61
ysun <ys.choi@astkorea.net>
2025-06-04 13:48:21 +0900

    logo import제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-          <Counter />
+      <Counter />
     </div>
   );
 }
